df27a2bf46cf5a1fd226f2c4a7eb808251b7d73f
Justice Conder <justiceconder@gmail.com>
2022-10-06 06:02:53 -0400

    first push from working local

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/conderj/pDAO-handbook/pdao-handbook/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/conderj/pDAO-handbook/pdao-handbook/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/conderj/pDAO-handbook/pdao-handbook/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/conderj/pDAO-handbook/pdao-handbook/src/css/custom.css'),
+];
